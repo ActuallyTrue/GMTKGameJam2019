6ba9b6bedc9f55ef79176df448e46c71c945b8a1
Java-Johnrh <35967122+Java-Johnrh@users.noreply.github.com>
2019-08-03 11:27:22 -0400

    Updated firing
    It's now inside of orbitcontrols on orbitter.

---
+++ GMTK Game Jam 2019/Assets/Scripts/OrbitControls.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitControls : MonoBehaviour
+{
+
+    public GameObject Bullet;
+    public float OrbitDistance = 3;
+    public float BulletSpeed = 30;
+    private Rigidbody2D BulletRB;
+
+    public bool repeatedFire = false;
+
+    bool LookingForPlayer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BulletRB = Bullet.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!repeatedFire)
+        {
+            if (Bullet.transform.parent != null)
+            {
+                if (Input.GetAxis("Fire1") != 0)
+                {
+                    Shoot();
+                }
+            }
+        }
+
+        else
+        {
+            if (Input.GetAxis("Fire1") != 0)
+            {
+                Shoot();
+            }
+        }
+
+        if (Input.GetAxis("Fire2") != 0)
+        {
+            ReturnProjectile();
+        }
+
+        if (LookingForPlayer)
+        {
+            Vector2 direction = gameObject.transform.position - Bullet.transform.position;
+            direction.Normalize();
+            BulletRB.isKinematic = true;
+            BulletRB.velocity = direction * BulletSpeed;
+            if ((Bullet.transform.position - gameObject.transform.position).magnitude <= OrbitDistance)
+            {
+                Bullet.transform.parent = gameObject.transform;
+                BulletRB.velocity = new Vector2(0, 0);
+                LookingForPlayer = false;
+
+                //if bullet too close, sets it to be a bit farther away 
+                if ((Bullet.transform.position - gameObject.transform.position).magnitude <= OrbitDistance - 1)
+                {
+                    Bullet.transform.localPosition = Bullet.transform.localPosition.normalized * OrbitDistance;
+                }
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        Bullet.transform.parent = null;
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Bullet.transform.position;
+        direction.Normalize();
+        BulletRB.isKinematic = false;
+        BulletRB.velocity = direction * BulletSpeed;
+        LookingForPlayer = false;
+    }
+
+    //tells the bullet to return to the player and makes it so the bullet is looking for the player to attach.
+    private void ReturnProjectile()
+    {
+        Vector2 direction = gameObject.transform.position - Bullet.transform.position;
+        direction.Normalize();
+        BulletRB.isKinematic = true;
+        BulletRB.velocity = direction * BulletSpeed;
+        LookingForPlayer = true;
+    }
+}
