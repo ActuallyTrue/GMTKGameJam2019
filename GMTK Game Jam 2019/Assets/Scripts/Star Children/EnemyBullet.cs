247155e42174fb62e329cfac54b8c5d8e031d460
ActuallyTrue <dotaigbe@icloud.com>
2019-08-02 23:25:03 -0500

    Groundwork for Lazy Starchild

---
+++ GMTK Game Jam 2019/Assets/Scripts/Star Children/EnemyBullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rBody2D;
+    public float TimeUntilDestroy = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody2D.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeUntilDestroy -= Time.deltaTime;
+
+        if (TimeUntilDestroy < 0) {
+            Object.Destroy(gameObject);
+            TimeUntilDestroy = 2;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.tag == "Player")
+        {
+            //Kill the player
+        }
+        else if (hitInfo.tag == "PlayerShield")
+        {
+            //Decrease shield health by one
+        }
+    }
+
+
+}
---
+++ GMTK Game Jam 2019/Assets/Scripts/Star Children/LazyStarChild.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazyStarChild : MonoBehaviour
+{
+
+    public float fireRate;
+    private float fireTimer;
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public GameObject Player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireRate = fireTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Aim(Player);
+        fireTimer -= Time.deltaTime;
+        if(fireTimer <= 0)
+        {
+            Shoot();
+            fireTimer = fireRate;
+        }
+    }
+
+
+    public void Aim(GameObject Player)
+    {
+        Vector3 targetDir = transform.position - Player.transform.position;
+        float zAngle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 180f;
+        transform.rotation = Quaternion.Euler(0, 0, zAngle);
+    }
+
+    public void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
