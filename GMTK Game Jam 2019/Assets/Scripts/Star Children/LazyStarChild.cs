6abd12103f90ce3b77019154cb336ce3ab50dd35
ActuallyTrue <dotaigbe@icloud.com>
2019-08-03 16:12:14 -0500

    LazyStarChild will only shoot when player is in range

--- GMTK Game Jam 2019/Assets/Scripts/Star Children/LazyStarChild.cs
+++ GMTK Game Jam 2019/Assets/Scripts/Star Children/LazyStarChild.cs
@@ -10,40 +10,46 @@ public class LazyStarChild : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPrefab;
     public GameObject Player;
+    public EnemyBehavior enemyBehavior;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        enemyBehavior = GetComponent<EnemyBehavior>();
         fireTimer = fireRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Aim(Player);
-        fireTimer -= Time.deltaTime;
-        if(fireTimer <= 0)
+        if (enemyBehavior.ShouldFire())
         {
-            Shoot();
-            fireTimer = fireRate;
+            Aim(Player);
+            fireTimer -= Time.deltaTime;
+            if (fireTimer <= 0)
+            {
+                Shoot();
+                fireTimer = fireRate;
+            }
         }
     }
 
 
-    public void Aim(GameObject Player)
+    private void Aim(GameObject Player)
     {
         Vector3 targetDir = transform.position - Player.transform.position;
         float zAngle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 180f;
         transform.rotation = Quaternion.Euler(0, 0, zAngle);
     }
 
-    public void Shoot()
+    private void Shoot()
     {
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 
-    public void Die()
+    private void Die()
     {
         Destroy(gameObject);
     }
