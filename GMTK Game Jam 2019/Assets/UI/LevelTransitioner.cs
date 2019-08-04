397c27cf91cbe61a7cda425c0e796fd9adb197e2
Timmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm <sonictimm@gmail.com>
2019-08-03 20:25:23 -0700

    MainMenu
    Added Main Menu Scene with tutorial

---
+++ GMTK Game Jam 2019/Assets/UI/LevelTransitioner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransitioner : MonoBehaviour
+{
+    public string destinationLevel;
+
+    //void OnCollisionEnter2D(Collider2D col)
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "PlayerBullet")
+        {
+            Debug.Log("Load Scene: " + destinationLevel);
+            SceneManager.LoadScene(destinationLevel);
+        }
+    }
+
+}
---
+++ GMTK Game Jam 2019/Assets/UI/OpacityModulater.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpacityModulater : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    bool increasing = false;
+
+    public float opacityMin, opacityMax, speed = 1.0f;
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+        {
+            Debug.Log("Sprite Renderer not found by OpacityModulater");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //print(spriteRenderer.color.a);
+
+        if (increasing)
+        {
+
+            if (spriteRenderer.color.a <= opacityMax)
+            {
+                spriteRenderer.color = new Color(1f,1f,1f,spriteRenderer.color.a + ( speed * Time.deltaTime));
+            }
+            else
+            {
+                increasing = false;
+            }
+        }
+        else
+        {
+
+            if (spriteRenderer.color.a >= opacityMin)
+            {
+                spriteRenderer.color = new Color(1f, 1f, 1f, spriteRenderer.color.a - (speed * Time.deltaTime));
+            }
+            else
+            {
+                increasing = true;
+            }
+        }
+    }
+}
