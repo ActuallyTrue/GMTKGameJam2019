cdb84259052c77864c9ac504b9f327cb6f5e07eb
Java-Johnrh <35967122+Java-Johnrh@users.noreply.github.com>
2019-08-04 14:37:02 -0400

    here we go.

---
+++ GMTK Game Jam 2019/Assets/Scripts/LevelRestart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelRestart : MonoBehaviour
+{
+
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.Equals(null))
+        {
+            StartCoroutine(RestartScene());
+        }
+    }
+
+    IEnumerator RestartScene()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
